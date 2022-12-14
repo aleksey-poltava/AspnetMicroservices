c273ff99657924b9f1e1b4509b42c5546850c148
Aleksey <poltavaaa@gmail.com>
2022-12-14 16:47:36 +0200

    Added UpdateOrderCommand

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName)
+                .NotEmpty()
+                .WithMessage("{UserName} is required")
+                .NotNull()
+                .MaximumLength(50).WithMessage($"UserName should not exceed 50 characters");
+
+            RuleFor(e => e.EmailAddress)
+                .NotEmpty().WithMessage("{EmailAddress} is required");
+
+            RuleFor(t => t.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice should not be empty")
+                .GreaterThan(0).WithMessage("{TotalPrice} should be greate than zero");
+        }
+    }
+}
+
