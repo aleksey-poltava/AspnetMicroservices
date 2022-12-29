64ce9d9798f1ad660cf930b08608b366e8d8e739
Aleksey <poltavaaa@gmail.com>
2022-12-29 16:42:41 +0200

    Added Ocelot project

---
+++ src/ApiGateways/OcelotApiGW/Program.cs
@@ -0,0 +1,29 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureLogging((hostingContext, logging) =>
+{
+    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));    
+    logging.AddConsole();
+    logging.AddDebug();
+});
+
+//IConfiguration configuration = new ConfigurationBuilder()
+//                            .AddJsonFile("ocelot.json")
+//                            .Build();
+
+//builder.Services.AddOcelot(configuration);
+
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+app.UseOcelot();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
