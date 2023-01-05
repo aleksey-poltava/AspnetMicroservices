273f661dfb18a07dfb53599d9ffe83b509ac3e82
Aleksey <poltavaaa@gmail.com>
2023-01-05 17:24:29 +0200

    Added Identity Server

---
+++ src/IdentityServer/IdentityServer/Config.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace IdentityServer
+{
+	public class Config
+	{
+        public static IEnumerable<Client> Clients =>
+         new Client[]
+         {
+             new Client
+                 {
+                 ClientId = "catalogClient",
+                 AllowedGrantTypes = GrantTypes.ClientCredentials,
+                 ClientSecrets =
+                     {
+                        new Secret("secret".Sha256())
+                     },
+                        AllowedScopes = { "catalogAPI" }
+                     }
+         };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+             new ApiScope[]
+             {
+                new ApiScope("catalogAPI", "Catalog API"),
+                new ApiScope("orderAPI", "Order API"),
+                new ApiScope("basketAPI", "Basket API")
+             };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+         new ApiResource[]
+         {
+         };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+         new IdentityResource[]
+         {
+         };
+
+        public static List<TestUser> TestUsers =>
+         new List<TestUser>
+         {
+         };
+
+    }
+}
+
---
+++ src/IdentityServer/IdentityServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using IdentityServer;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Add Identity Server4 DI
+builder.Services.AddIdentityServer()
+     .AddInMemoryClients(Config.Clients)
+     .AddInMemoryApiScopes(Config.ApiScopes)     
+     .AddDeveloperSigningCredential();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseIdentityServer();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
