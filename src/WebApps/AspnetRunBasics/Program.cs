89d6e5e16a8d2dfa4d9b49cd44a6391d6ea103ac
Aleksey <poltavaaa@gmail.com>
2023-01-09 15:11:12 +0200

    Migrated ApNetBasics to IHttpClientService and Ocelot Gateway

--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -9,9 +9,7 @@ namespace AspnetRunBasics
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-        
-            host.Run();
+            CreateHostBuilder(args).Build().Run(); 
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
